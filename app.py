8e0941e1b7986667dca3b8b9eb997a2a3e1c238c
rtjshreyd <shreyanshece1041@gmail.com>
2021-03-26 14:03:10 +0530

    Redis Connection error rectified

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+ 
+from controllers.therm import therm_controller
+ 
+app = Flask(__name__, static_url_path='/static/index.html')
+app.register_blueprint(therm_controller)
+CORS(app)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
